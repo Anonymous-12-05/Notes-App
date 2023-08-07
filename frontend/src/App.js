358eda70931dc9c152305b50b770b7b12f0104c7
Joel Jacob John <achuthengone03@gmail.com>
2023-08-07 19:57:04 +0530

    Final Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 import NotePage from './pages/NotePage';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
@@ -12,12 +12,14 @@ import {
 function App() {
   return (
     <Router>
-    <div className="App">
+    <div className='container dark'>
+      <div className="App  ">
       <Header/>
       <Routes>
       <Route path="/" exact element={<NotesListPage/>}/>
       <Route path="/note/:id" element={<NotePage/>}></Route>
       </Routes>
+      </div>
     </div>
     </Router>
   );
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,30 +1,114 @@
-
 import React, { useState, useEffect } from "react";
-import {useParams} from 'react-router-dom'
-
+import { useParams, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 
 const NotePage = () => {
   const params = useParams();
-  let [note, setNote] = useState(null);
+  const navigate = useNavigate();
+  const [note, setNote] = useState(null);
 
   useEffect(() => {
     getNote();
-  });
+  },[params.id]);
 
-  let getNote = async () => {
+  const getNote = async () => {
     try {
-      let response = await fetch(`/api/notes/${params.id}`);
-      let data = await response.json();
+      if(params.id==='new') return
+      const response = await fetch(`/api/notes/${params.id}`);
+      const data = await response.json();
       console.log(data);
       setNote(data);
     } catch (error) {
       console.error("Error retrieving note:", error);
     }
-  };  
+  }; 
+
+  const createNote = async () => {
+    try {
+      fetch(`/api/notes/`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      });
+    } catch (error) {
+      console.error("Error creating note:", error);
+    }
+  }
+  
+  const updateNote = async () => {
+    try {
+      const response = await fetch(`/api/notes/${params.id}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      });
+      if (response.ok) {
+        console.log("Note updated successfully!");
+      } else {
+        console.error("Error updating note:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error updating note:", error);
+    }
+  }
+
+  const handleTextareaChange = (e) => {
+    setNote({ ...note, body: e.target.value }); // Update only the 'body' property
+  };
+
+  
+  const deleteNote = async () => {
+    try {
+      const response = await fetch(`/api/notes/${params.id}`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      navigate('/');
+      if (response.ok) {
+        console.log("Note updated successfully!");
+      } else {
+        console.error("Error updating note:", response.statusText);
+      }
+      
+    } catch (error) {
+      console.error("Error deleting note:", error);
+    }
+  }
+
+  const handleSubmit = () => {
+    if(params.id!=='new' && note.body===""){
+      deleteNote();
+    }
+    else if(params.id!=='new'){
+      updateNote();
+    }
+    else if(params.id==='new' && note.body!==null){
+        createNote();
+    }
+    
+    navigate('/'); // Navigate to the desired route
+  }
 
   return (
-    <div>
-      {note?.body}
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <ArrowLeft onClick={handleSubmit} />
+        </h3>
+        {params.id!=='new'?(<button onClick={deleteNote} >Delete</button>):<button onClick={handleSubmit}>Done</button>}
+        
+
+      </div>
+      <textarea
+        onChange={handleTextareaChange}
+        value={note?.body || ""} // Use 'value' instead of 'defaultValue'
+      ></textarea>
     </div>
   );
 };
